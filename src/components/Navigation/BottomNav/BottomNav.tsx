13204b6f381dfb38ccdcdcf90b65a6983f7d4b8c
Christoph Stenglein <christoph@cstenglein.com>
2021-05-01 19:34:41 +0200

    Clean up SideDrawer & BottomNav; comment out btc & lightning pages

--- src/components/Navigation/BottomNav/BottomNav.tsx
+++ src/components/Navigation/BottomNav/BottomNav.tsx
@@ -1,33 +1,36 @@
 import { NavLink } from 'react-router-dom';
-import { ReactComponent as BitcoinIcon } from '../../../assets/bitcoin-circle.svg';
 import { ReactComponent as HomeLogo } from '../../../assets/home.svg';
-import { ReactComponent as LightningIcon } from '../../../assets/lightning.svg';
+import { ReactComponent as ServicesIcon } from '../../../assets/services.svg';
 import { ReactComponent as SettingsIcon } from '../../../assets/settings.svg';
 
-const BottomNav = () => (
-  <footer className='md:hidden z-10 flex flex-wrap items-center justify-evenly h-16 w-full shadow-inner fixed bottom-0 border-t-2 bg-white dark:bg-gray-800 transition-colors'>
-    <NavLink to='/' exact activeClassName='text-blue-700 dark:text-yellow-500' className='dark:text-gray-300'>
-      <HomeLogo className='w-8 h-8 mx-auto' />
-      <div className='text-center mx-1'>Home</div>
-    </NavLink>
-    <NavLink to='bitcoin' activeClassName='text-blue-700 dark:text-yellow-500' className='dark:text-gray-300'>
-      {/* TODO: Change Bitcoin icon; doesn't like to be imported twice somehow & thus is ugly here */}
+const BottomNav = () => {
+  const navLinkClasses = 'dark:text-gray-300';
+  const navLinkActiveClasses = 'text-blue-700 dark:text-yellow-500';
+
+  return (
+    <footer className='md:hidden z-10 flex flex-wrap items-center justify-evenly h-16 w-full shadow-inner fixed bottom-0 border-t-2 bg-white dark:bg-gray-800 transition-colors'>
+      <NavLink to='/' exact className={navLinkClasses} activeClassName={navLinkActiveClasses}>
+        <HomeLogo className='w-8 h-8 mx-auto' />
+        <div className='text-center mx-1'>Home</div>
+      </NavLink>
+      {/* <NavLink to='bitcoin' className={navLinkClasses} activeClassName={navLinkActiveClasses}>
       <BitcoinIcon className='w-8 h-8 mx-auto' />
       <div className='text-center mx-1'>Bitcoin</div>
-    </NavLink>
-    <NavLink to='lightning' activeClassName='text-blue-700 dark:text-yellow-500' className='dark:text-gray-300'>
+    </NavLink> */}
+      {/* <NavLink to='lightning' className={navLinkClasses} activeClassName={navLinkActiveClasses}'>
       <LightningIcon className='w-8 h-8 mx-auto' />
       <div className='text-center mx-1'>Lightning</div>
-    </NavLink>
-    {/* <NavLink to='services' activeClassName='text-blue-700 dark:text-yellow-500' className='dark:text-gray-300'>
+    </NavLink> */}
+      <NavLink to='services' className={navLinkClasses} activeClassName={navLinkActiveClasses}>
         <ServicesIcon className='w-8 h-8 mx-auto' />
         <div className='text-center mx-1'>Services</div>
-      </NavLink> */}
-    <NavLink to='settings' activeClassName='text-blue-700 dark:text-yellow-500' className='dark:text-gray-300'>
-      <SettingsIcon className='w-8 h-8 mx-auto' />
-      <div className='text-center mx-1'>Settings</div>
-    </NavLink>
-  </footer>
-);
+      </NavLink>
+      <NavLink to='settings' className={navLinkClasses} activeClassName={navLinkActiveClasses}>
+        <SettingsIcon className='w-8 h-8 mx-auto' />
+        <div className='text-center mx-1'>Settings</div>
+      </NavLink>
+    </footer>
+  );
+};
 
 export default BottomNav;
--- src/components/Navigation/SideDrawer/SideDrawer.tsx
+++ src/components/Navigation/SideDrawer/SideDrawer.tsx
@@ -1,54 +1,36 @@
-import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { ReactComponent as BitcoinIcon } from '../../../assets/bitcoin-circle.svg';
 import { ReactComponent as HomeLogo } from '../../../assets/home.svg';
-import { ReactComponent as LightningIcon } from '../../../assets/lightning.svg';
+import { ReactComponent as ServicesIcon } from '../../../assets/services.svg';
 import { ReactComponent as SettingsIcon } from '../../../assets/settings.svg';
 
-export class SideDrawer extends Component {
-  render(): JSX.Element {
-    return (
-      <nav className='hidden md:inline-block content-container md:w-3/12 xl:w-2/12 px-2 text-center pt-8 shadow-lg bg-white dark:bg-gray-800 transition-colors border-t-2'>
-        <NavLink
-          to='/'
-          exact
-          activeClassName='text-blue-700 dark:text-yellow-500'
-          className='inline-block py-4 w-full dark:text-gray-300'
-        >
-          <HomeLogo className='inline-block w-8 h-8 mx-auto' />
-          <div className='inline-block text-center mx-1'>Home</div>
-        </NavLink>
-        <NavLink
-          to='bitcoin'
-          activeClassName='text-blue-700 dark:text-yellow-500'
-          className='inline-block py-4 w-full dark:text-gray-300'
-        >
-          <BitcoinIcon className='inline-block w-8 h-8 mx-auto' />
-          <div className='inline-block text-center mx-1'>Bitcoin</div>
-        </NavLink>
-        <NavLink
-          to='lightning'
-          activeClassName='text-blue-700 dark:text-yellow-500'
-          className='inline-block py-4 w-full dark:text-gray-300'
-        >
-          <LightningIcon className='inline-block w-8 h-8 mx-auto' />
-          <div className='inline-block text-center mx-1'>Lightning</div>
-        </NavLink>
-        {/* <NavLink to='services' activeClassName='inline-block py-4 w-full text-blue-700 dark:text-yellow-500' className='dark:text-gray-300'>
+export const SideDrawer = () => {
+  const navLinkClasses = 'inline-block py-4 w-full dark:text-gray-300';
+  const navLinkActiveClasses = 'text-blue-700 dark:text-yellow-500';
+
+  return (
+    <nav className='hidden md:inline-block content-container md:w-3/12 xl:w-2/12 px-2 text-center pt-8 shadow-lg bg-white dark:bg-gray-800 transition-colors border-t-2'>
+      <NavLink to='/' exact className={navLinkClasses} activeClassName={navLinkActiveClasses}>
+        <HomeLogo className='inline-block w-8 h-8 mx-auto' />
+        <div className='inline-block text-center mx-1'>Home</div>
+      </NavLink>
+      {/* <NavLink to='bitcoin' className={navLinkClasses} activeClassName={navLinkActiveClasses}>
+        <BitcoinIcon className='inline-block w-8 h-8 mx-auto' />
+        <div className='inline-block text-center mx-1'>Bitcoin</div>
+      </NavLink> */}
+      {/* <NavLink to='lightning' className={navLinkClasses} activeClassName={navLinkActiveClasses}>
+        <LightningIcon className='inline-block w-8 h-8 mx-auto' />
+        <div className='inline-block text-center mx-1'>Lightning</div>
+      </NavLink> */}
+      <NavLink to='services' className={navLinkClasses} activeClassName={navLinkActiveClasses}>
         <ServicesIcon className='inline-block w-8 h-8 mx-auto' />
         <div className='inline-block text-center mx-1'>Services</div>
-      </NavLink> */}
-        <NavLink
-          to='settings'
-          activeClassName='text-blue-700 dark:text-yellow-500'
-          className='inline-block py-4 w-full dark:text-gray-300'
-        >
-          <SettingsIcon className='inline-block w-8 h-8 mx-auto' />
-          <div className='inline-block text-center mx-1'>Settings</div>
-        </NavLink>
-      </nav>
-    );
-  }
-}
+      </NavLink>
+      <NavLink to='settings' className={navLinkClasses} activeClassName={navLinkActiveClasses}>
+        <SettingsIcon className='inline-block w-8 h-8 mx-auto' />
+        <div className='inline-block text-center mx-1'>Settings</div>
+      </NavLink>
+    </nav>
+  );
+};
 
 export default SideDrawer;
