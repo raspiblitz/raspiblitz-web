caa01d0285f16231d2fa4b3786e50ae63fb0c4a7
Christoph Stenglein <christoph@cstenglein.com>
2021-05-02 19:13:15 +0200

    comment out bitcoin & lightning routes; remove outline in sendmodal, fix title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Bitcoin from './components/Bitcoin/Bitcoin';
 import Home from './components/Home/Home';
-import Lightning from './components/Lightning/Lightning';
 import BottomNav from './components/Navigation/BottomNav/BottomNav';
 import Header from './components/Navigation/Header/Header';
 import SideDrawer from './components/Navigation/SideDrawer/SideDrawer';
@@ -21,12 +18,12 @@ function App() {
             <Route exact path='/'>
               <Home />
             </Route>
-            <Route path='/bitcoin'>
+            {/* <Route path='/bitcoin'>
               <Bitcoin />
             </Route>
             <Route path='/lightning'>
               <Lightning />
-            </Route>
+            </Route> */}
             <Route path='/services'>
               <Services />
             </Route>
