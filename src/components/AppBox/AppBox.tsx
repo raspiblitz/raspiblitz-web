522b278477908e65de8a52c4def2c57537b96bac
Christoph Stenglein <christoph@cstenglein.com>
2021-05-12 20:53:04 +0200

    use li instead of div in AppBox

--- src/components/AppBox/AppBox.tsx
+++ src/components/AppBox/AppBox.tsx
@@ -9,12 +9,12 @@ const AppBox: FC<AppList> = (props) => {
 
   return (
     <DashboardBox name={'Apps'} addText={`${onlineAppCount} / ${appCount} Apps online`} logo={logo}>
-      <ul className='px-4 py-5 max-h-60 md:max-h-80 xl:max-h-112 overflow-y-auto'>
+      <ul className='px-4 pt-5 max-h-60 md:max-h-80 xl:max-h-112 overflow-y-auto'>
         {!props.apps.length && <div className='flex justify-center items-center'>No Apps installed!</div>}
         {props.apps.map((service: App, index: number) => {
           return (
-            <div key={index} className='flex py-4'>
-              <div className='w-9/12  font-bold text-center'>{service.name}</div>
+            <li key={index} className='flex py-4'>
+              <div className='w-9/12 font-bold text-center'>{service.name}</div>
               <div className='w-3/12 flex justify-end items-center'>
                 <div
                   className={`w-3 h-3 border border-black align-middle rounded-full ${
@@ -23,7 +23,7 @@ const AppBox: FC<AppList> = (props) => {
                 ></div>
                 <div className='italic text-gray-600 dark:text-gray-400'>&nbsp; {service.status}</div>
               </div>
-            </div>
+            </li>
           );
         })}
       </ul>
