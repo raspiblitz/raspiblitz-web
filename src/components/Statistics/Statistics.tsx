49315628f8f87740a27dd97948534cd7c2915ee8
Christoph Stenglein <christoph@cstenglein.com>
2021-05-13 10:40:00 +0200

    move statistics into categories; nicer scrollbars

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -4,23 +4,38 @@ import DashboardBox from '../../container/DashboardBox/DashboardBox';
 
 const Statistics: FC = () => {
   const logo = <LightningLogo className='w-10 h-10' />;
-  const liClassNames = 'text-center px-4 py-3 hover:bg-gray-300 dark:hover:bg-gray-600';
+
+  const categoryClasses = 'w-full py-1 text-center bg-gray-200 font-bold shadow-sm dark:bg-gray-500';
+  const itemClasses = 'text-center px-4 py-3 hover:bg-gray-300 dark:hover:bg-gray-600';
 
   return (
     <DashboardBox name={'Info'} addText={'Raspiblitz Stats'} logo={logo}>
-      <ul className='pt-5 max-h-60 md:max-h-80 xl:max-h-112 overflow-y-auto'>
-        <li className={liClassNames}>bitcoin v0.21.0 mainnet 15 peers</li>
-        <li className={liClassNames}>Blocks 683322/683322 Sync OK 100.00%</li>
-        <li className={liClassNames}>LND 0.12.1-beta wallet 1111111111111 sat</li>
-        <li className={liClassNames}>10/11 Channels 0302020202 sat 10 peers</li>
-        <li className={liClassNames}>Fee Report (D-W-M): 1-2-3 sat</li>
-        {/* Add Copy functionality */}
-        <li className={liClassNames}>Tor: pg6mmjiyjmcrsslvykfwnntlaru7p5svn6y2ymmju6nubxndf4pscryd.onion:1234</li>
-        <li className={liClassNames}>SSH: admin@192.168.0.100</li>
-        <li className={liClassNames}>Web admin: http://192.168.0.111:3000</li>
-        <li className={liClassNames}>X Peers</li>
-        <li className={liClassNames}>X Peers</li>
-      </ul>
+      <div className='pt-5' />
+      <div className='max-h-60 md:max-h-80 xl:max-h-112 overflow-y-auto'>
+        {/* Connect Stats */}
+        <div className={categoryClasses}>Connect</div>
+        <ul>
+          {/* TODO: Add Copy functionality */}
+          <li className={`${itemClasses} overflow-hidden overflow-ellipsis`}>
+            Tor: pg6mmjiyjmcrsslvykfwnntlaru7p5svn6y2ymmju6nubxndf4pscryd.onion:1234
+          </li>
+          <li className={itemClasses}>SSH: admin@192.168.0.100</li>
+          <li className={itemClasses}>Web admin: http://192.168.0.111:3000</li>
+        </ul>
+        {/* Bitcoin Stats */}
+        <div className={categoryClasses}>Bitcoin</div>
+        <ul className='max-h-60 md:max-h-80 xl:max-h-112 overflow-y-auto'>
+          <li className={itemClasses}>bitcoin v0.21.0 mainnet 15 peers connected</li>
+          <li className={itemClasses}>Sync: Block 683322/683322</li>
+        </ul>
+        {/* Lightning Stats */}
+        <div className={categoryClasses}>Lightning</div>
+        <ul className='max-h-60 md:max-h-80 xl:max-h-112 overflow-y-auto'>
+          <li className={itemClasses}>LND 0.12.1-beta</li>
+          <li className={itemClasses}>10/11 Channels 0302020202 sat 10 peers</li>
+          <li className={itemClasses}>Fee Report (D-W-M): 1-2-3 sat</li>
+        </ul>
+      </div>
     </DashboardBox>
   );
 };
