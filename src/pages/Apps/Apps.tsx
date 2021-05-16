3d451dfa440f9ed2f845e8e0cdfe324020c24d73
Christoph Stenglein <christoph@cstenglein.com>
2021-05-16 18:18:05 +0200

    add empty dashboard Boxes for Apps page

--- src/pages/Apps/Apps.tsx
+++ src/pages/Apps/Apps.tsx
@@ -1,5 +1,14 @@
+import DashboardBox from '../../container/DashboardBox/DashboardBox';
+
 export const Apps = () => {
-  return <div className='content-container w-full dark:text-white'>Apps</div>;
+  return (
+    <div className='content-container w-full dark:text-white transition-colors'>
+      <div className='h-full grid gap-4 grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-2 xl:grid-cols-2 xl:grid-rows-3'>
+        <DashboardBox addText='' logo={<div></div>} name='Installed' />
+        <DashboardBox addText='' logo={<div></div>} name='Available Apps' />
+      </div>
+    </div>
+  );
 };
 
 export default Apps;
