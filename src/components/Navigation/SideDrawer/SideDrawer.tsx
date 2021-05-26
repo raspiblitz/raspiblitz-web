aae6958f28e93c752cb47b848fba12c8fda7d6b2
Christoph Stenglein <christoph@cstenglein.com>
2021-05-26 20:51:17 +0200

    align sidedrawer items with logo

--- src/components/Navigation/SideDrawer/SideDrawer.tsx
+++ src/components/Navigation/SideDrawer/SideDrawer.tsx
@@ -5,28 +5,22 @@ import { ReactComponent as HomeLogo } from '../../../assets/home.svg';
 import { ReactComponent as SettingsIcon } from '../../../assets/settings.svg';
 
 export const SideDrawer: FC = () => {
-  const navLinkClasses = 'inline-block py-4 w-full dark:text-gray-300 flex items-center justify-center opacity-60';
+  const navLinkClasses = 'inline-block py-4 pl-6 w-full dark:text-gray-300 flex items-center opacity-60';
   const navLinkActiveClasses = 'text-yellow-500 dark:text-yellow-500 opacity-100';
 
   return (
-    <nav className='hidden md:inline-block content-container md:w-2/12 xl:w-2/12 px-2 text-center pt-8 shadow-lg bg-white dark:bg-gray-800 transition-colors'>
+    <nav className='hidden md:inline-block content-container md:w-2/12 xl:w-2/12 px-2 pt-8 shadow-lg bg-white dark:bg-gray-800 transition-colors'>
       <NavLink to='/home' className={navLinkClasses} activeClassName={navLinkActiveClasses}>
-        <div className='w-1/2 flex justify-end'>
-          <HomeLogo className='inline-block w-8 h-8' />
-        </div>
-        <div className='w-1/2 flex jusify-start text-center mx-1'>Home</div>
+        <HomeLogo className='inline-block w-8 h-8' />
+        <div className='w-1/2 text-center mx-1'>Home</div>
       </NavLink>
       <NavLink to='apps' className={navLinkClasses} activeClassName={navLinkActiveClasses}>
-        <div className='w-1/2 flex justify-end'>
-          <AppIcon className='inline-block w-8 h-8' />
-        </div>
-        <div className='w-1/2 flex jusify-start text-center mx-1'>Apps</div>
+        <AppIcon className='inline-block w-8 h-8' />
+        <div className='w-1/2 text-center mx-1'>Apps</div>
       </NavLink>
       <NavLink to='settings' className={navLinkClasses} activeClassName={navLinkActiveClasses}>
-        <div className='w-1/2 flex justify-end'>
-          <SettingsIcon className='inline-block w-8 h-8' />
-        </div>
-        <div className='w-1/2 flex jusify-start text-center mx-1'>Settings</div>
+        <SettingsIcon className='inline-block w-8 h-8' />
+        <div className='w-1/2 mx-1 text-center'>Settings</div>
       </NavLink>
     </nav>
   );
