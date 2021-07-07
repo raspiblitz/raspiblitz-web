cc03cd3ef3b6aaa68fab012deb33f9ddefcef79e
Christoph Stenglein <christoph@cstenglein.com>
2021-07-07 20:36:21 +0200

    center dropdown

--- src/components/Navigation/Header/DropdownMenu/DropdownMenu.tsx
+++ src/components/Navigation/Header/DropdownMenu/DropdownMenu.tsx
@@ -1,8 +1,10 @@
 import { forwardRef, useContext } from 'react';
+import { useTranslation } from 'react-i18next';
 import { AppContext } from '../../../../store/app-context';
 import Toggle from '../../../Shared/Toggle/Toggle';
 
 const DropdownMenu = forwardRef<HTMLDivElement>((_, ref) => {
+  const { t } = useTranslation();
   const appCtx = useContext(AppContext);
   const unitActive = appCtx.unit === 'Sat';
   const darkActive = appCtx.darkMode;
@@ -10,14 +12,14 @@ const DropdownMenu = forwardRef<HTMLDivElement>((_, ref) => {
   return (
     <div
       ref={ref}
-      className='absolute right-0 border border-black rounded-lg w-56 h-28 z-10 bg-white dark:bg-gray-800 dark:border-gray-300'
+      className='flex absolute right-0 border border-black rounded-lg w-56 h-28 z-10 bg-white dark:bg-gray-800 dark:border-gray-300'
     >
       <div className='flex w-full text-center justify-center flex-col items-center'>
         <div className='w-full py-3'>
-          <Toggle toggleText='Display Sats' active={unitActive} toggleFn={appCtx.toggleUnit} />
+          <Toggle toggleText={t('navigation.display_sats')} active={unitActive} toggleFn={appCtx.toggleUnit} />
         </div>
         <div className='w-full py-3'>
-          <Toggle toggleText='Dark Mode' active={darkActive} toggleFn={appCtx.toggleDarkMode} />
+          <Toggle toggleText={t('navigation.dark_mode')} active={darkActive} toggleFn={appCtx.toggleDarkMode} />
         </div>
       </div>
     </div>
