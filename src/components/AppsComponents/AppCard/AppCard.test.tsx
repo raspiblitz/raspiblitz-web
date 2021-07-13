e092e778bcb140d03cc353574cb3f50347f8fcb5
Christoph Stenglein <christoph@cstenglein.com>
2021-07-13 20:37:16 +0200

    add testconfig and AppCard test

---
+++ src/components/AppsComponents/AppCard/AppCard.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import { AppCard } from './AppCard';
+import i18n from '../../../i18n/test_config';
+import { I18nextProvider } from 'react-i18next';
+
+const bluuub = () => {};
+
+const basic = {
+  id: '123',
+  description: 'Hi',
+  installed: false,
+  installing: false,
+  name: 'd',
+  onInstall: bluuub,
+  onOpenDetails: bluuub
+};
+
+describe('AppCard', () => {
+  test('display install button if installed is false', async () => {
+    render(
+      <I18nextProvider i18n={i18n}>
+        <AppCard {...basic} />
+      </I18nextProvider>
+    );
+    expect(await screen.findByText('apps.install')).toBeDefined();
+  });
+
+  test('display open button if installed & address is available', async () => {
+    render(
+      <I18nextProvider i18n={i18n}>
+        <AppCard {...basic} installed={true} address='abc' />
+      </I18nextProvider>
+    );
+    expect(await screen.findByText('apps.open')).toBeDefined();
+  });
+
+  test('display no_page button if installed & address is not available', async () => {
+    render(
+      <I18nextProvider i18n={i18n}>
+        <AppCard {...basic} installed={true} />
+      </I18nextProvider>
+    );
+    expect(await screen.findByText('apps.no_page')).toBeDefined();
+  });
+});
---
+++ src/i18n/test_config.ts
@@ -0,0 +1,15 @@
+// this is the i18n config used for testing
+
+import i18n from 'i18next';
+import 'react-i18next';
+import { initReactI18next } from 'react-i18next';
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false
+  },
+  resources: { en: { translations: {} } }
+});
+
+export default i18n;
