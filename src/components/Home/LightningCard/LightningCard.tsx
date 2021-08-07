ff316f69e7337033d0d144f175464b136c7fa2ef
Christoph Stenglein <christoph@cstenglein.com>
2021-08-07 12:13:18 +0200

    add loadingbox to dashboard, more semantic html

--- src/components/Home/LightningCard/LightningCard.tsx
+++ src/components/Home/LightningCard/LightningCard.tsx
@@ -1,37 +1,44 @@
 import { FC, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { AppContext } from '../../../store/app-context';
+import LoadingBox from '../../Shared/LoadingBox/LoadingBox';
 
 export const LightningCard: FC<LightningCardProps> = (props) => {
   const { t } = useTranslation();
   const appCtx = useContext(AppContext);
 
-  const balance = appCtx.unit === 'BTC' ? props.channelBalance : props.channelBalance * 100_000_000;
+  const { channelBalance, version, channelOnline, channelTotal, status } = props;
+
+  if (!channelBalance || !version || !channelOnline || !channelTotal || !status) {
+    return <LoadingBox />;
+  }
+
+  const balance = appCtx.unit === 'BTC' ? channelBalance : channelBalance * 100_000_000;
 
   return (
-    <div className='p-5 h-full'>
-      <div className='bd-card transition-colors'>
+    <div className='h-full p-5'>
+      <div className='bd-card'>
         <h2 className='font-bold text-lg'>{t('home.lightning')}</h2>
         <div className='flex overflow-hidden py-4'>
           <div className='w-1/2'>
-            <h4 className='text-sm text-gray-500 dark:text-gray-200'>{t('home.version')}</h4>
-            <div>{props.version}</div>
+            <h6 className='text-sm text-gray-500 dark:text-gray-200'>{t('home.version')}</h6>
+            <p>{version}</p>
           </div>
           <div className='w-1/2'>
-            <h4 className='text-sm text-gray-500 dark:text-gray-200'>{t('home.status')}</h4>
-            <div>{props.status}</div>
+            <h6 className='text-sm text-gray-500 dark:text-gray-200'>{t('home.status')}</h6>
+            <p>{status}</p>
           </div>
         </div>
         <div className='flex overflow-hidden py-4'>
           <div className='w-1/2'>
-            <h4 className='text-sm text-gray-500 dark:text-gray-200'>{t('home.channel')}</h4>
-            <div>{`${props.channelOnline} / ${props.channelTotal}`}</div>
+            <h6 className='text-sm text-gray-500 dark:text-gray-200'>{t('home.channel')}</h6>
+            <p>{`${channelOnline} / ${channelTotal}`}</p>
           </div>
           <div className='w-1/2'>
-            <h4 className='text-sm text-gray-500 dark:text-gray-200'>{t('home.channel_balance')}</h4>
-            <div>
+            <h6 className='text-sm text-gray-500 dark:text-gray-200'>{t('home.channel_balance')}</h6>
+            <p>
               {balance.toLocaleString()} {appCtx.unit}
-            </div>
+            </p>
           </div>
         </div>
       </div>
---
+++ src/components/Shared/LoadingBox/LoadingBox.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
+
+export const LoadingBox: FC = () => (
+  <div className='h-full p-5'>
+    <div className='bd-card flex justify-center items-center'>
+      <LoadingSpinner />
+    </div>
+  </div>
+);
+
+export default LoadingBox;
