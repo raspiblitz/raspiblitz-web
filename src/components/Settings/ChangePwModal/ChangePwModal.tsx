7ca6c41fe197bda7799d8edc65a9a1a5d6021fdd
Christoph Stenglein <christoph@cstenglein.com>
2021-08-20 12:11:15 +0200

    small improvements

--- src/components/Settings/ChangePwModal/ChangePwModal.tsx
+++ src/components/Settings/ChangePwModal/ChangePwModal.tsx
@@ -3,6 +3,7 @@ import { createPortal } from 'react-dom';
 import { useTranslation } from 'react-i18next';
 import ModalDialog from '../../../container/ModalDialog/ModalDialog';
 import { instance } from '../../../util/interceptor';
+import { MODAL_ROOT } from '../../../util/util';
 import LoadingSpinner from '../../Shared/LoadingSpinner/LoadingSpinner';
 
 const ChangePwModal: FC<ChangePwModalProps> = (props) => {
@@ -30,50 +31,57 @@ const ChangePwModal: FC<ChangePwModalProps> = (props) => {
     setNewPassword(event.target.value);
   };
 
+  if (isLoading) {
+    return createPortal(
+      <ModalDialog close={props.onClose}>
+        <h3 className='font-bold'>{t('settings.change_pw')}</h3>
+        <LoadingSpinner />
+      </ModalDialog>,
+      MODAL_ROOT
+    );
+  }
+
   return createPortal(
     <ModalDialog close={props.onClose}>
       <h3 className='font-bold'>{t('settings.change_pw')}</h3>
-      {isLoading && <LoadingSpinner />}
-      {!isLoading && (
-        <div className='my-5 flex flex-col justify-center text-center items-center'>
-          <div className='w-full md:w-10/12 py-1'>
-            <label htmlFor='oldpw' className='label-underline'>
-              {t('settings.old_pw')}
-            </label>
-            <input
-              id='oldpw'
-              className='w-full input-underline'
-              type='password'
-              value={oldPassword}
-              onChange={changeOldPasswordHandler}
-              required
-            />
-          </div>
-          <div className='w-full md:w-10/12 py-1'>
-            <label htmlFor='newpw' className='label-underline'>
-              {t('settings.new_pw')}
-            </label>
-            <input
-              id='newpw'
-              className='w-full input-underline'
-              type='password'
-              value={newPassword}
-              onChange={changeNewPasswordHandler}
-              required
-            />
-          </div>
-          <div className='flex flex-col xl:flex-row w-full md:w-2/3 pt-2 text-white'>
-            <button className={btnClasses} onClick={props.onClose}>
-              {t('settings.cancel')}
-            </button>
-            <button className={btnClasses} onClick={changePasswordHandler}>
-              {t('settings.change_pw')}
-            </button>
-          </div>
+      <div className='my-5 flex flex-col justify-center text-center items-center'>
+        <div className='w-full md:w-10/12 py-1'>
+          <label htmlFor='oldpw' className='label-underline'>
+            {t('settings.old_pw')}
+          </label>
+          <input
+            id='oldpw'
+            className='w-full input-underline'
+            type='password'
+            value={oldPassword}
+            onChange={changeOldPasswordHandler}
+            required
+          />
         </div>
-      )}
+        <div className='w-full md:w-10/12 py-1'>
+          <label htmlFor='newpw' className='label-underline'>
+            {t('settings.new_pw')}
+          </label>
+          <input
+            id='newpw'
+            className='w-full input-underline'
+            type='password'
+            value={newPassword}
+            onChange={changeNewPasswordHandler}
+            required
+          />
+        </div>
+        <div className='flex flex-col xl:flex-row w-full md:w-2/3 pt-2 text-white'>
+          <button className={btnClasses} onClick={props.onClose}>
+            {t('settings.cancel')}
+          </button>
+          <button className={btnClasses} onClick={changePasswordHandler}>
+            {t('settings.change_pw')}
+          </button>
+        </div>
+      </div>
     </ModalDialog>,
-    document.getElementById('modal-root')!
+    MODAL_ROOT
   );
 };
 
