67fee70a07140b0e23298ec29ffa028a17c508ce
Christoph Stenglein <christoph@cstenglein.com>
2021-09-27 07:03:15 +0200

    blur connection address by default, add toggle button

--- src/components/Home/ConnectionCard/ConnectionCard.tsx
+++ src/components/Home/ConnectionCard/ConnectionCard.tsx
@@ -1,11 +1,23 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ReactComponent as EyeIcon } from '../../../assets/eye.svg';
 import LoadingBox from '../../Shared/LoadingBox/LoadingBox';
+
+const HIDDEN_TEXT = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 
 export const ConnectionCard: FC<ConnectionCardProps> = (props) => {
   const { t } = useTranslation();
-
   const { sshAddress, torAddress } = props;
+  const [showTorAddr, setShowTorAddr] = useState(false);
+  const [showSshAddr, setShowSshAddr] = useState(false);
+
+  const toggleTorAddrHandler = () => {
+    setShowTorAddr((prev) => !prev);
+  };
+
+  const toggleSshAddrHandler = () => {
+    setShowSshAddr((prev) => !prev);
+  };
 
   if (!sshAddress || !torAddress) {
     return <LoadingBox />;
@@ -16,19 +28,25 @@ export const ConnectionCard: FC<ConnectionCardProps> = (props) => {
       <div className='bd-card transition-colors'>
         <div className='font-bold text-lg'>{t('home.conn_details')}</div>
         <div className='flex flex-col overflow-hidden py-4'>
-          <h6 className='text-sm text-gray-500 dark:text-gray-200'>{t('home.tor')}</h6>
+          <h6 className='text-sm text-gray-500 dark:text-gray-200'>
+            {t('home.tor')}&nbsp;
+            <EyeIcon onClick={toggleTorAddrHandler} className='inline-block align-top h-6 w-6 cursor-pointer' />
+          </h6>
           <a
-            className='overflow-hidden overflow-ellipsis text-blue-400 underline'
+            className={`${showTorAddr ? 'overflow-hidden overflow-ellipsis text-blue-400 underline' : 'text-blur'}`}
             href={`//${torAddress}`}
             target='_blank'
             rel='noreferrer'
           >
-            {props.torAddress}
+            {showTorAddr ? props.torAddress : HIDDEN_TEXT}
           </a>
         </div>
         <div className='flex flex-col overflow-hidden py-4'>
-          <h6 className='text-sm text-gray-500 dark:text-gray-200'>{t('home.ssh')}</h6>
-          <p>{sshAddress}</p>
+          <h6 className='text-sm text-gray-500 dark:text-gray-200'>
+            {t('home.ssh')}&nbsp;
+            <EyeIcon onClick={toggleSshAddrHandler} className='inline-block align-top h-6 w-6 cursor-pointer' />
+          </h6>
+          <p className={showSshAddr ? '' : 'text-blur'}>{showSshAddr ? sshAddress : HIDDEN_TEXT}</p>
         </div>
       </div>
     </div>
