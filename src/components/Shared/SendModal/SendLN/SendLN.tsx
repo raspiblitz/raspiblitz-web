8dd27b46bb7ba62e145963d59bebd1b9fa27e8d1
escapedcat <github@htmlcss.de>
2021-11-05 13:16:01 +0800

    feat(sendLn): use validity and add error-message #50

--- src/components/Shared/SendModal/SendLN/SendLN.tsx
+++ src/components/Shared/SendModal/SendLN/SendLN.tsx
@@ -1,4 +1,11 @@
-import { ChangeEvent, FC, FormEvent, useContext, useState } from "react";
+import {
+  ChangeEvent,
+  FC,
+  FormEvent,
+  useContext,
+  useState,
+  useRef,
+} from "react";
 import { useTranslation } from "react-i18next";
 import { AppContext } from "../../../../store/app-context";
 
@@ -7,34 +14,76 @@ const SendLn: FC<SendLnProps> = (props) => {
   const { balance, onChangeInvoice, onConfirm } = props;
   const { t } = useTranslation();
 
+  const invoiceInput = useRef<HTMLInputElement>(null);
   const [isFormValid, setIsFormValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const validateInput = (event: ChangeEvent<HTMLInputElement>) => {
-    // TODO: check wallet balance
-    setIsFormValid(event.target.validity.valid)
-  }
+  const validateInput = (validity: HTMLInputElement["validity"]) => {
+    setIsFormValid(validity.valid);
+    setErrorMessage("");
+
+    if (validity.valueMissing) {
+      setErrorMessage(t("forms.validation.lnInvoice.required"));
+      return false;
+    }
+
+    if (validity.patternMismatch) {
+      setErrorMessage(t("forms.validation.lnInvoice.patternMismatch"));
+      return false;
+    }
+
+    return true;
+  };
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    validateInput(event.target.validity);
+    onChangeInvoice(event);
+  };
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const validity = invoiceInput?.current?.validity;
+
+    if (validity) {
+      const isFormValid = validateInput(validity);
+
+      if (isFormValid) {
+        onConfirm(event);
+      }
+    }
+  };
 
   return (
-    <form onSubmit={onConfirm}>
+    <form onSubmit={onSubmit} noValidate>
       <h3 className="text-xl font-bold">{t("wallet.send_lightning")}</h3>
+
       <p className="my-5">
         <span className="font-bold">{t("wallet.balance")}:&nbsp;</span>
         {balance} {appCtx.unit}
       </p>
+
       <label className="label-underline" htmlFor="invoiceInput">
         {t("wallet.invoice")}
       </label>
+
       <input
         id="invoiceInput"
+        ref={invoiceInput}
         type="text"
-        onChange={onChangeInvoice}
-        onBlur={validateInput}
+        onChange={onChange}
         required
         pattern="(lnbc|lntb|lntbs|lnbcrt)\w+"
-        className={isFormValid ? "input-underline":"input-error"}
-
+        className={isFormValid ? "input-underline" : "input-error"}
+        placeholder="lnbc..."
       />
-      <button type="submit" className="bd-button p-3 my-3" disabled={!isFormValid}>
+
+      <p className="text-red-500">{errorMessage}</p>
+
+      <button
+        type="submit"
+        className="bd-button p-3 my-3"
+        disabled={!isFormValid}
+      >
         {t("wallet.send")}
       </button>
     </form>
