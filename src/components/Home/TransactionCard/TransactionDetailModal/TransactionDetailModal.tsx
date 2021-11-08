44c56ab217896718513e66437de09336b8946daa
Christoph Stenglein <christoph@cstenglein.com>
2021-11-08 21:32:55 +0100

    feat(txdetail): fix null pointer, more semantic html

--- src/components/Home/TransactionCard/TransactionDetailModal/TransactionDetailModal.tsx
+++ src/components/Home/TransactionCard/TransactionDetailModal/TransactionDetailModal.tsx
@@ -9,27 +9,26 @@ export const TransactionDetailModal: FC<TransactionDetailModalProps> = (
   props
 ) => {
   const { t } = useTranslation();
-
   const { transaction } = props;
 
   return (
     <ModalDialog close={props.close}>
       <section className="flex flex-col">
-        <div className="font-extrabold">{t("tx.tx_details")}</div>
-        {transaction.category === "onchain" && (
-          <a
-            className="text-blue-400 underline break-all py-2"
-            target="_blank"
-            rel="noreferrer"
-            href={`https://mempool.space/tx/${transaction.id}`}
-          >
-            {t("tx.mempool")}
-          </a>
+        <h4 className="font-extrabold">{t("tx.tx_details")}</h4>
+        {transaction?.category === "onchain" && (
+          <>
+            <a
+              className="text-blue-400 underline break-all py-2"
+              target="_blank"
+              rel="noreferrer"
+              href={`https://mempool.space/tx/${transaction.id}`}
+            >
+              {t("tx.mempool")}
+            </a>
+            <OnchainDetails details={transaction} />
+          </>
         )}
-        {transaction.category === "onchain" && (
-          <OnchainDetails details={transaction} />
-        )}
-        {transaction.category === "ln" && <LNDetails details={transaction} />}
+        {transaction?.category === "ln" && <LNDetails details={transaction} />}
       </section>
     </ModalDialog>
   );
