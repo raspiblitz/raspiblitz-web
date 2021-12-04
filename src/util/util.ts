c86f8aaf2dade97ec818e37ef374e362cb2024b0
Christoph Stenglein <christoph@cstenglein.com>
2021-12-04 16:42:06 +0100

    use arrow function for parseJwt function

--- src/util/util.ts
+++ src/util/util.ts
@@ -49,15 +49,13 @@ export function checkPropsUndefined(props: object): boolean {
 }
 
 // see https://stackoverflow.com/a/38552302
-export function parseJwt(token: string) {
+export function parseJwt(token: string): any {
   const base64Url = token.split(".")[1];
   const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
   const jsonPayload = decodeURIComponent(
     atob(base64)
       .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
+      .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
       .join("")
   );
 
