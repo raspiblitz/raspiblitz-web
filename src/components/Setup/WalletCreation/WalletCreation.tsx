e4b639080054349f7b3c674a1f2280e75a9bdda2
Christoph Stenglein <christoph@cstenglein.com>
2022-03-20 10:41:52 +0100

    remove all setup steps except welcome; create setup container

--- src/components/Setup/WalletCreation/WalletCreation.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import SetupContainer from "../../../container/SetupContainer/SetupContainer";
-
-const WalletCreation: FC = (props) => {
-  return <SetupContainer>WalletCreation </SetupContainer>;
-};
-
-export default WalletCreation;
