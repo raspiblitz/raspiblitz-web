2f0127ca981374eda48dcd592a70b74dc79ba1b3
Christoph Stenglein <christoph@cstenglein.com>
2022-04-01 21:50:38 +0200

    better contrast for dark mode modals

--- src/container/ModalBackground/ModalBackground.tsx
+++ src/container/ModalBackground/ModalBackground.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 const ModalBackground: FC = (props) => (
-  <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-gray-600 bg-opacity-20 dark:bg-gray-300 dark:bg-opacity-20">
+  <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-gray-600 bg-opacity-20 dark:bg-gray-700 dark:bg-opacity-60">
     {props.children}
   </div>
 );
