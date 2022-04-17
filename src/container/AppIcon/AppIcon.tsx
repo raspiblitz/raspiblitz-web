de74ee00d8891c8bdbbc27db800102d5dc632991
Christoph Stenglein <christoph@cstenglein.com>
2022-04-17 09:17:11 +0200

    move cloud.svg & apps into public folder; simplify app img loading

--- src/container/AppIcon/AppIcon.tsx
+++ src/container/AppIcon/AppIcon.tsx
@@ -1,26 +1,20 @@
-import { FC, useEffect, useState } from "react";
+import type { FC } from "react";
 
 type Props = {
   appId: string;
 };
 
 const AppIcon: FC<Props> = ({ appId }) => {
-  const [iconImg, setIconImg] = useState("");
-  useEffect(() => {
-    import(`../../assets/apps/logos/${appId}.png`)
-      .then((image) => {
-        setIconImg(image.default);
-      })
-      .catch((_) => {
-        // use fallback icon if image for id doesn't exist
-        import("../../assets/cloud.svg")
-          .then((img) => setIconImg(img.default))
-          .catch((_) => {
-            // do nothing if cloud image doesn't exist either
-          });
-      });
-  });
-  return <img className="max-h-16" src={iconImg} alt={`${appId} Logo`} />;
+  return (
+    <img
+      className="max-h-16"
+      src={`/assets/apps/logos/${appId}.png`}
+      onError={(e) => {
+        (e.target as HTMLImageElement).src = "/assets/cloud.svg";
+      }}
+      alt={`${appId} Logo`}
+    />
+  );
 };
 
 export default AppIcon;
