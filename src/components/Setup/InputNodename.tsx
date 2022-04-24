027f85bd939c38d4ed0a29abb25c80b35f6b4d5e
Christoph Stenglein <christoph@cstenglein.com>
2022-04-24 09:32:45 +0200

    InputNodename: display input errors correctly; fix translation typo

--- src/components/Setup/InputNodename.tsx
+++ src/components/Setup/InputNodename.tsx
@@ -1,75 +1,88 @@
 import { ChangeEvent, FC, useState } from "react";
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { ReactComponent as X } from "../../assets/X.svg";
 import { ReactComponent as ArrowRight } from "../../assets/arrow-sm-right.svg";
+import { ReactComponent as X } from "../../assets/X.svg";
 import SetupContainer from "../../container/SetupContainer/SetupContainer";
+import InputField from "../Shared/InputField/InputField";
 
 export type Props = {
   callback: (nodename: string | null) => void;
 };
 
+interface IFormInputs {
+  inputNodeName: string;
+}
+
 const InputNodename: FC<Props> = ({ callback }) => {
-  const [nodeName, setNodename] = useState("");
+  const [inputNodeName, setInputNodeName] = useState("");
   const { t } = useTranslation();
   // later use {t("setup.set_lang")}
 
   const changeNodenameHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setNodename(event.target.value);
+    setInputNodeName(event.target.value);
   };
 
   const continueHandler = () => {
-    // check is password is valid
-    if (!nodeName) {
-      alert(t("setup.nodename_error_empty"));
-      return;
-    }
-    if (nodeName.length < 4) {
-      alert(t("setup.nodename_error_length"));
-      return;
-    }
-    if (!nodeName.match(/^[a-zA-Z0-9]*$/)) {
-      alert(t("setup.nodename_error_chars"));
-      return;
-    }
-    callback(nodeName);
+    callback(inputNodeName);
   };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<IFormInputs>({
+    mode: "onChange",
+  });
 
   return (
     <SetupContainer>
-      <h2 className="text-center text-lg font-bold">
-        {t("setup.nodename_input")}
-      </h2>
-      <div className="flex flex-col items-center justify-center">
-        <div className="w-full py-1 md:w-10/12">
-          <label htmlFor="oldpw" className="label-underline">
-            {t("setup.nodename_name")}
-          </label>
-          <input
-            id="oldpw"
-            className="input-underline w-full"
-            type="text"
-            value={nodeName}
-            onChange={changeNodenameHandler}
-            required
-          />
-        </div>
-        <div className="mt-5 flex gap-2">
-          <button
-            onClick={() => callback(null)}
-            className="flex items-center rounded  bg-red-500 px-2 text-white shadow-xl hover:bg-red-400 disabled:bg-gray-400"
-          >
-            <X className="inline h-6 w-6" />
-            <span className="p-2">{t("setup.cancel")}</span>
-          </button>
-          <button
-            onClick={continueHandler}
-            className="bd-button flex items-center px-2"
-          >
-            <span className="p-2">Continue</span>
-            <ArrowRight className="inline h-6 w-6" />
-          </button>
-        </div>
-      </div>
+      <section className="flex h-full flex-col items-center justify-center p-8">
+        <h2 className="text-center text-lg font-bold">
+          {t("setup.nodename_input")}
+        </h2>
+        <form
+          onSubmit={handleSubmit(continueHandler)}
+          className="flex h-full w-full flex-col py-1 md:w-10/12"
+        >
+          <article className="my-auto">
+            <InputField
+              {...register("inputNodeName", {
+                required: t("setup.nodename_error_empty"),
+                onChange: changeNodenameHandler,
+                pattern: {
+                  value: /^[a-zA-Z0-9]*$/,
+                  message: t("setup.nodename_error_chars"),
+                },
+                minLength: {
+                  value: 4,
+                  message: t("setup.nodename_error_length"),
+                },
+              })}
+              label={t("setup.nodename_name")}
+              errorMessage={errors.inputNodeName}
+            />
+          </article>
+          <article className="mt-10 flex flex-col items-center justify-center gap-10 md:flex-row">
+            <button
+              type="button"
+              onClick={() => callback(null)}
+              className="flex items-center rounded  bg-red-500 px-2 text-white shadow-xl hover:bg-red-400 disabled:bg-gray-400"
+            >
+              <X className="inline h-6 w-6" />
+              <span className="p-2">{t("setup.cancel")}</span>
+            </button>
+            <button
+              disabled={!isValid}
+              type="submit"
+              className="bd-button flex items-center px-2 disabled:bg-gray-400"
+            >
+              <span className="p-2">Continue</span>
+              <ArrowRight className="inline h-6 w-6" />
+            </button>
+          </article>
+        </form>
+      </section>
     </SetupContainer>
   );
 };
