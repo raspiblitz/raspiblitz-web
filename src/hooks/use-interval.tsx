52339fd235c84c1e4437361ca68f968c4b1d519b
Christoph Stenglein <christoph@cstenglein.com>
2022-05-18 06:59:01 +0200

    poll transactions, add use-interval

---
+++ src/hooks/use-interval.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+
+/**
+ * Calls function repeatedly until unmounted.
+ * For reference see {@link https://overreacted.io/making-setinterval-declarative-with-react-hooks/}
+ * @param callback the function you want to call repeatedly
+ * @param delay delay in milliseconds
+ */
+export const useInterval = (callback: () => unknown, delay: number) => {
+  const savedCallback = useRef<() => unknown>();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const tick = () => {
+      savedCallback.current?.();
+    };
+    const id = setInterval(tick, delay);
+    return () => {
+      clearInterval(id);
+    };
+  }, [delay]);
+};
