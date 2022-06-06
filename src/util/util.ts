09011b739a0f0c072baea486bc389e8c430dca5b
Christoph Stenglein <christoph@cstenglein.com>
2022-06-06 10:39:17 +0200

    simplify parseJwt

--- src/util/util.ts
+++ src/util/util.ts
@@ -61,18 +61,8 @@ export function checkPropsUndefined(props: object): boolean {
   return someUndefined;
 }
 
-// see https://stackoverflow.com/a/38552302
 export function parseJwt(token: string): TokenPayload {
-  const base64Url = token.split(".")[1];
-  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  const jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split("")
-      .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
-      .join("")
-  );
-
-  return JSON.parse(jsonPayload);
+  return JSON.parse(atob(token.split(".")[1]));
 }
 
 export function enableGutter(): void {
