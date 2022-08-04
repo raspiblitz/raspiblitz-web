b0bc5187c370c4f27af3b65dc2cb9f704bea5cc6
Christoph Stenglein <christoph@cstenglein.com>
2022-08-04 17:13:08 +0200

    add test for ConfirmSendModal

--- src/components/Shared/SendModal/ConfirmSendModal/ConfirmSendModal.test.tsx
+++ src/components/Shared/SendModal/ConfirmSendModal/ConfirmSendModal.test.tsx
@@ -2,9 +2,12 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../../../../i18n/test_config";
+import { rest, server } from "../../../../testServer";
 import { TxType } from "../../SwitchTxType/SwitchTxType";
 import type { Props } from "./ConfirmSendModal";
 import ConfirmSendModal from "./ConfirmSendModal";
+
+const closeSpy = jest.fn();
 
 const basicProps: Props = {
   amount: 0,
@@ -12,7 +15,7 @@ const basicProps: Props = {
     "lnbcrt10u1pscxuktpp5k4hp6wxafdaqfhk84krlt26q80dfdg5df3cdagwjpr5v8xc7s5qqdpz2phkcctjypykuan0d93k2grxdaezqcn0vgxqyjw5qcqp2sp5ndav50eqfh32xxpwd4wa645hevumj7ze5meuajjs40vtgkucdams9qy9qsqc34r4wlyytf68xvt540gz7yq80wsdhyy93dgetv2d2x44dhtg4fysu9k8v0aec8r649tcgtu5s9xths93nuxklvf93px6gnlw2h7u0gq602rww",
   back: () => {},
   balance: 100,
-  close: () => {},
+  close: closeSpy,
   comment: "",
   expiry: 36000,
   fee: "",
@@ -81,6 +84,36 @@ describe("ConfirmSendModal", () => {
       expect(
         screen.getByRole("button", { name: "check.svg settings.confirm" })
       ).not.toBeDisabled();
+    });
+
+    test("amountInput correctly sends mSat", async () => {
+      server.use(
+        rest.post("/api/v1/lightning/send-payment", (req, res, ctx) => {
+          console.log(req.url.searchParams.get("amount_msat"));
+          if (req.url.searchParams.get("amount_msat") === "10000") {
+            return res(ctx.status(200));
+          } else {
+            return res(ctx.status(500));
+          }
+        })
+      );
+      const user = userEvent.setup();
+      setup();
+
+      const amountInput = screen.getByLabelText(
+        "wallet.amount"
+      ) as HTMLInputElement;
+
+      await user.type(amountInput, "10");
+      await waitFor(() => expect(amountInput).not.toHaveClass("input-error"));
+
+      const confirmBtn = screen.getByRole("button", {
+        name: "check.svg settings.confirm",
+      });
+
+      await user.click(confirmBtn);
+
+      expect(closeSpy).toHaveBeenCalledWith(true);
     });
   });
 
