9ecb6826e7146cfafccfe81bdfb9e97fcd04085d
Christoph Stenglein <christoph@cstenglein.com>
2022-12-19 20:10:29 +0100

    improve ApiError typing. add response code & text to unknown error

--- src/utils/checkError.ts
+++ src/utils/checkError.ts
@@ -1,11 +1,8 @@
+import { AxiosError } from "axios";
 import { t } from "i18next";
 
 export interface ApiError {
-  response: {
-    data: {
-      detail: string | ApiErrorDetails | ApiErrorDetails[];
-    };
-  };
+  detail: string | ApiErrorDetails | ApiErrorDetails[];
 }
 
 export interface ApiErrorDetails {
@@ -20,20 +17,23 @@ export interface ApiErrorDetails {
  * https://github.com/fusion44/blitz_api/issues/123
  * @returns The error with the translated prefix "An error occurred" (error text not yet translated)
  */
-export function checkError(err: ApiError): string {
-  const responseData = err.response.data;
+export function checkError(err: AxiosError<ApiError>): string {
+  const responseData = err.response?.data;
 
   if (typeof responseData?.detail === "string") {
     return `${t("login.error")}: ${responseData.detail}`;
   }
 
   if (Array.isArray(responseData?.detail)) {
-    return `${t("login.error")}: ${responseData.detail[0].msg}`;
+    return `${t("login.error")}: ${responseData?.detail[0].msg}`;
   }
 
   if (typeof responseData?.detail?.msg === "string") {
     return `${t("login.error")}: ${responseData.detail.msg}`;
   }
 
-  return `${t("login.error")}: Unknown error`;
+  return `${t("login.error")}: ${t("login.unknown_error", {
+    code: err.response?.status,
+    statusText: err.response?.statusText,
+  })}`;
 }
