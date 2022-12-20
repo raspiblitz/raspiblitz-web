89fe79419c05609e4c5f677fe0a59a972cf310cb
Christoph Stenglein <christoph@cstenglein.com>
2022-12-20 10:16:20 +0100

    (backend-mock): add version to installed_app_status

--- backend-mock/sse/installed_app_status.js
+++ backend-mock/sse/installed_app_status.js
@@ -3,12 +3,48 @@ const util = require("./util");
 const appStatus = () => {
   console.info("sending installed_app_status");
   util.sendSSE("installed_app_status", [
-    { id: "rtl", installed: false, status: "offline", error: "" },
-    { id: "thunderhub", installed: false, status: "offline", error: "" },
-    { id: "lnbits", installed: false, status: "offline", error: "" },
-    { id: "btc-rpc-explorer", installed: false, status: "offline", error: "" },
-    { id: "btcpayserver", installed: false, status: "offline", error: "" },
-    { id: "mempool", installed: false, status: "offline", error: "" },
+    {
+      id: "rtl",
+      installed: false,
+      status: "offline",
+      version: "v1.0.0",
+      error: "",
+    },
+    {
+      id: "thunderhub",
+      installed: false,
+      version: "v1.0.0",
+      status: "offline",
+      error: "",
+    },
+    {
+      id: "lnbits",
+      installed: false,
+      version: "v1.0.0",
+      status: "offline",
+      error: "",
+    },
+    {
+      id: "btc-rpc-explorer",
+      installed: false,
+      version: "v1.0.0",
+      status: "offline",
+      error: "",
+    },
+    {
+      id: "btcpayserver",
+      installed: false,
+      version: "v1.0.0",
+      status: "offline",
+      error: "",
+    },
+    {
+      id: "mempool",
+      installed: false,
+      version: "v1.0.0",
+      status: "offline",
+      error: "",
+    },
   ]);
 };
 
