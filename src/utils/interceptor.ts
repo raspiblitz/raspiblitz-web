42f7a423e4e960f78d7240f96bbace099e013771
Christoph Stenglein <christoph@cstenglein.com>
2023-01-26 19:39:45 +0100

    (dev): update dependencies

--- src/utils/interceptor.ts
+++ src/utils/interceptor.ts
@@ -1,15 +1,12 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 import { ACCESS_TOKEN } from ".";
 
 export const instance = axios.create({ baseURL: "/api/v1" });
 
-instance.interceptors.request.use((config: AxiosRequestConfig) => {
+instance.interceptors.request.use((config) => {
   const token = localStorage.getItem(ACCESS_TOKEN);
   if (token) {
-    if (!config.headers) {
-      config.headers = {};
-    }
-    config.headers["Authorization"] = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
