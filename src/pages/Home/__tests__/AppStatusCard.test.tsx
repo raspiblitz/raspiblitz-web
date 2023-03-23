2df4be913d147fb04a20525b74f88b1943337761
Christoph Stenglein <christoph@cstenglein.com>
2023-03-23 17:43:26 +0100

    add tests for AppStatusCard

---
+++ src/pages/Home/__tests__/AppStatusCard.test.tsx
@@ -0,0 +1,41 @@
+import { AppStatus } from "models/app-status";
+import AppStatusCard from "pages/Home/AppStatusCard";
+import { render, screen } from "test-utils";
+
+const testApp: AppStatus = {
+  id: "specter",
+  installed: true,
+  version: "1.0.0",
+  address: "http://127.0.0.1",
+  hiddenService: "http://hiddenservice.onion",
+  error: "",
+};
+
+describe("AppStatusCard", () => {
+  it("should link to 'address'", () => {
+    render(<AppStatusCard app={testApp} />);
+
+    const appCard = screen.getAllByRole("link");
+    expect(appCard[0].getAttribute("href")).toEqual("http://127.0.0.1");
+  });
+
+  it("should link to 'hiddenService' if window.location is an onion address", () => {
+    console.log(global.window);
+    const oldWindow = global.window;
+    global.window = Object.create(window);
+    Object.defineProperty(window, "location", {
+      value: {
+        hostname: "http://someplace.onion",
+      },
+      writable: true,
+    });
+    render(<AppStatusCard app={testApp} />);
+
+    const appCard = screen.getAllByRole("link");
+    console.log(window.location.hostname);
+    expect(appCard[0].getAttribute("href")).toEqual(
+      "http://hiddenservice.onion"
+    );
+    global.window = oldWindow;
+  });
+});
