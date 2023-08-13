7df4be22e35159b980194df267669083719b8a37
escapedcat <github@htmlcss.de>
2023-08-13 14:35:18 +0800

    refactor(openChannel): switch to new modal component

--- src/pages/Home/OpenChannelModal.tsx
+++ src/pages/Home/OpenChannelModal.tsx
@@ -1,4 +1,3 @@
-import { LinkIcon } from "@heroicons/react/24/outline";
 import AvailableBalance from "components/AvailableBalance";
 import { ChangeEvent, FC, useContext, useState } from "react";
 import { createPortal } from "react-dom";
@@ -6,15 +5,14 @@ import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { toast } from "react-toastify";
 import AmountInput from "../../components/AmountInput";
-import ButtonWithSpinner from "../../components/ButtonWithSpinner/ButtonWithSpinner";
 import InputField from "../../components/InputField";
 import Message from "../../components/Message";
 import { AppContext } from "../../context/app-context";
-import ModalDialog from "../../layouts/ModalDialog";
 import { MODAL_ROOT } from "../../utils";
 import { checkError } from "../../utils/checkError";
 import { convertMSatToSat, stringToNumber } from "../../utils/format";
 import { instance } from "../../utils/interceptor";
+import Modal from "../../layouts/Modal";
 
 interface IFormInputs {
   nodeUri: string;
@@ -38,7 +36,7 @@ const OpenChannelModal: FC<Props> = ({ balance, onClose }) => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid },
+    formState: { errors },
   } = useForm<IFormInputs>({
     mode: "onChange",
   });
@@ -55,7 +53,7 @@ const OpenChannelModal: FC<Props> = ({ balance, onClose }) => {
             node_URI: data.nodeUri,
             target_confs: +data.feeRate,
           },
-        }
+        },
       )
       .then(() => {
         toast.success(t("home.channel_opened"), { theme });
@@ -72,62 +70,59 @@ const OpenChannelModal: FC<Props> = ({ balance, onClose }) => {
   const convertedBalance = balance ? convertMSatToSat(balance) : 0;
 
   return createPortal(
-    <ModalDialog close={onClose}>
-      <h2 className="text-lg font-bold">{t("home.open_channel")}</h2>
-      <article className="my-5">
+    <Modal
+      closeFunc={onClose}
+      submitFunc={handleSubmit(openChannelHandler)}
+      title={t("home.open_channel")}
+      submitLabel={t("home.open_channel")}
+      is-loading={isLoading}
+    >
+      <fieldset className="mb-5 sm:w-96">
         <AvailableBalance balance={convertedBalance!} />
 
-        <form onSubmit={handleSubmit(openChannelHandler)}>
-          <InputField
-            {...register("nodeUri", {
-              required: t("forms.validation.node_uri.required"),
-            })}
-            label={t("home.node_uri")}
-            errorMessage={errors.nodeUri}
-          />
-          <AmountInput
-            amount={amount}
-            errorMessage={errors.fundingAmount}
-            register={register("fundingAmount", {
-              required: t("forms.validation.amount.required"),
-              validate: {
-                greaterThanZero: (val) =>
-                  stringToNumber(val) > 0 ||
-                  t("forms.validation.amount.required"),
-              },
-              onChange: changeAmountHandler,
-            })}
-          />
-          <div className="flex justify-around py-8 md:mx-auto md:w-1/2">
-            <label htmlFor="targetConf" className="font-bold">
-              {t("tx.fee_rate")}
-            </label>
-            <select
-              id="targetConf"
-              defaultValue={4}
-              {...register("feeRate")}
-              className="rounded bg-yellow-500 p-1 text-white"
-            >
-              <option value={1}>{t("home.urgent")}</option>
-              <option value={4}>{t("home.normal")}</option>
-              <option value={20}>{t("home.slow")}</option>
-            </select>
-          </div>
+        <InputField
+          {...register("nodeUri", {
+            required: t("forms.validation.node_uri.required"),
+          })}
+          label={t("home.node_uri")}
+          errorMessage={errors.nodeUri}
+        />
 
-          <ButtonWithSpinner
-            type="submit"
-            className="bd-button p-2"
-            loading={isLoading}
-            disabled={!isValid}
-            icon={<LinkIcon className="mx-1 h-6 w-6" />}
+        <AmountInput
+          amount={amount}
+          errorMessage={errors.fundingAmount}
+          register={register("fundingAmount", {
+            required: t("forms.validation.amount.required"),
+            validate: {
+              greaterThanZero: (val) =>
+                stringToNumber(val) > 0 ||
+                t("forms.validation.amount.required"),
+            },
+            onChange: changeAmountHandler,
+          })}
+        />
+
+        <div>
+          <label htmlFor="targetConf" className="label-underline">
+            {t("tx.fee_rate")}
+          </label>
+
+          <select
+            id="targetConf"
+            defaultValue={4}
+            {...register("feeRate")}
+            className="p-2 rounded shadow-md dark:bg-gray-600"
           >
-            {t("home.open_channel")}
-          </ButtonWithSpinner>
-        </form>
+            <option value={1}>{t("home.urgent")}</option>
+            <option value={4}>{t("home.normal")}</option>
+            <option value={20}>{t("home.slow")}</option>
+          </select>
+        </div>
+
         {error && <Message message={error} />}
-      </article>
-    </ModalDialog>,
-    MODAL_ROOT
+      </fieldset>
+    </Modal>,
+    MODAL_ROOT,
   );
 };
 
