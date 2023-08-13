e069071f5b6423da962848a1f675cf0de76c3e34
escapedcat <github@htmlcss.de>
2023-08-13 14:34:39 +0800

    feat(modal): increase headlien font-size

--- src/layouts/Modal.tsx
+++ src/layouts/Modal.tsx
@@ -84,7 +84,7 @@ const Modal: FC<Props> = ({
                       <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                         <Dialog.Title
                           as="h3"
-                          className="text-base font-semibold leading-6 text-gray-900 dark:text-white"
+                          className="text-lg font-semibold leading-6 text-gray-900 dark:text-white"
                         >
                           {title}
                         </Dialog.Title>
