9476ac710b2e64f61a678dcb8650ef3e7e15d3ab
JP <jpyepezimc@gmail.com>
2024-03-08 21:52:04 +1300

    feat: add refresh address button and translations

--- src/pages/Home/ReceiveModal/__tests__/ReceiveModal.test.tsx
+++ src/pages/Home/ReceiveModal/__tests__/ReceiveModal.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "test-utils";
 import { http, server, HttpResponse } from "@/testServer";
 import ReceiveModal from "../ReceiveModal";
 
-beforeAll(() => {
+beforeEach(() => {
   server.use(
     http.post("/api/lightning/new-address", () => {
       return new HttpResponse("bcrt1qvh74klc36lefsdgq5r2d44vwxxzkdsch0hhyrz", {
@@ -23,5 +23,36 @@ describe("ReceiveModal", () => {
     await user.click(onChainBtn);
 
     expect(await screen.findByRole("img")).toBeDefined();
+  });
+
+  test("Retrieves a new address upon clicking the refresh button", async () => {
+    const user = userEvent.setup();
+    render(<ReceiveModal onClose={() => {}} />);
+
+    const onChainBtn = screen.getByRole("button", { name: "wallet.on_chain" });
+
+    await user.click(onChainBtn);
+
+    // one-time address override
+    const newMockAddress = "muZ3nH1uH2U5JBhZRvYU46MdChbCifSghQ";
+    server.use(
+      http.post(
+        "/api/lightning/new-address",
+        () => {
+          return new HttpResponse(newMockAddress, {
+            status: 200,
+          });
+        },
+        { once: true },
+      ),
+    );
+
+    const refreshBtn = await screen.findByRole("button", {
+      name: "wallet.refresh",
+    });
+    await user.click(refreshBtn);
+
+    expect(await screen.findByRole("img")).toBeInTheDocument();
+    expect(await screen.findByText(newMockAddress)).toBeInTheDocument();
   });
 });
