56f0416f8f0163a07468feea23dbe944e4aa0cbd
escapedcat <github@htmlcss.de>
2024-05-17 18:15:44 +0200

    feat(syncScreen): design, use nextui

--- src/pages/Setup/SyncScreen.tsx
+++ src/pages/Setup/SyncScreen.tsx
@@ -14,7 +14,7 @@ import Message from "@/components/Message";
 import SetupContainer from "@/layouts/SetupContainer";
 import { checkError } from "@/utils/checkError";
 import { instance } from "@/utils/interceptor";
-import ProgressCircle from "./ProgressCircle";
+import { Input, Button, Progress } from "@nextui-org/react";
 
 export interface InputData {
   data: SyncData | any;
@@ -90,84 +90,89 @@ const SyncScreen: FC<InputData> = ({ data, callback }) => {
   const lnWalletLocked = lnEnabled && data.ln_default_locked === "1";
   const lnWalletUnlocked = lnEnabled && data.ln_default_locked === "0";
 
+  const btcDefaultReady = data.btc_default_ready === "1";
+  const btcDefaultSyncPercentage = +data.btc_default_sync_percentage;
+
   return (
     <SetupContainer>
-      <section className="flex h-full flex-col items-center justify-center md:p-8">
-        <h2 className="text-center text-lg font-bold">
+      <section className="flex h-full w-96 max-w-3xl flex-col items-center justify-center gap-y-8 lg:p-8">
+        <h2 className="text-center text-2xl font-semibold">
           {t("setup.sync_headline")}
         </h2>
-        <div className="flex h-full w-full flex-col py-1 md:w-10/12">
-          <article className="my-auto flex flex-col items-center justify-between md:flex-row">
-            <div className="my-8 flex w-full justify-center md:my-0 md:w-1/2">
-              <ProgressCircle
-                progress={+data.btc_default_sync_percentage}
-                starting={data.btc_default_ready !== "1"}
-              />
-            </div>
-            <div className="flex w-full flex-col justify-center md:w-1/2">
-              {lnWalletUnlocked && (
-                <article>
-                  <h6 className="text-sm text-gray-500 dark:text-gray-300">
-                    {t("home.lightning")}
-                  </h6>
-                  <div className="flex justify-around">
-                    <p className="flex gap-2">
-                      {t("setup.started")}:
-                      {data.ln_default_ready === "1" ? (
-                        <CheckCircleIcon className="inline h-6 w-6" />
-                      ) : (
-                        <XCircleIcon className="inline h-6 w-6" />
-                      )}
-                    </p>
-                    <p className="flex">
-                      {t("setup.restarts")}: {data.system_count_start_lightning}
-                    </p>
-                  </div>
-                </article>
-              )}
-              {lnWalletLocked && (
-                <form
-                  className="flex flex-col justify-center"
-                  onSubmit={handleSubmit(unlockWallet)}
+
+        <div className="w-full">
+          <Progress
+            label={
+              btcDefaultReady
+                ? `${t("setup.sync_bitcoin_sync")}: ${btcDefaultSyncPercentage}%`
+                : `${t("setup.sync_bitcoin_starting")}...`
+            }
+            value={btcDefaultSyncPercentage}
+            isStriped
+          />
+
+          {/* {lnWalletUnlocked && ( */}
+          <p className="mt-6">
+            {data.ln_default_ready === "1" ? (
+              <>
+                <CheckCircleIcon className="inline h-6 w-auto text-success " />{" "}
+                {t("home.lightning")} {t("setup.started")}
+              </>
+            ) : (
+              <>
+                <XCircleIcon className="inline h-6 w-auto text-danger" />{" "}
+                {t("home.lightning")} {t("setup.not_started")}
+              </>
+            )}
+            , {t("setup.restarts")}: {data.system_count_start_lightning}
+          </p>
+          {/*  )} */}
+
+          {/* {lnWalletLocked && (
+            <>
+              <form
+                className="flex flex-col justify-center"
+                onSubmit={handleSubmit(unlockWallet)}
+              >
+                <InputField
+                  {...register("passwordInput", {
+                    required: t("setup.password_error_empty"),
+                    onChange: changePasswordHandler,
+                  })}
+                  type="password"
+                  label={t("setup.sync_wallet_info")}
+                  disabled={runningUnlock}
+                  errorMessage={errors.passwordInput}
+                  value={password}
+                />
+                <ButtonWithSpinner
+                  type="submit"
+                  onClick={unlockWallet}
+                  loading={runningUnlock}
+                  disabled={!isValid}
+                  icon={<LockOpenIcon className="mx-1 inline h-6 w-6" />}
+                  className="bd-button my-5 p-2"
                 >
-                  <InputField
-                    {...register("passwordInput", {
-                      required: t("setup.password_error_empty"),
-                      onChange: changePasswordHandler,
-                    })}
-                    type="password"
-                    label={t("setup.sync_wallet_info")}
-                    disabled={runningUnlock}
-                    errorMessage={errors.passwordInput}
-                    value={password}
-                  />
-                  <ButtonWithSpinner
-                    type="submit"
-                    onClick={unlockWallet}
-                    loading={runningUnlock}
-                    disabled={!isValid}
-                    icon={<LockOpenIcon className="mx-1 inline h-6 w-6" />}
-                    className="bd-button my-5 p-2"
-                  >
-                    {t("setup.sync_wallet_unlock")}
-                  </ButtonWithSpinner>
-                </form>
-              )}
-              {lnWalletLocked && error && <Message message={error} />}
-            </div>
-          </article>
-          <article className="mx-auto flex flex-col justify-center">
-            <button
-              onClick={() => callback("shutdown", null)}
-              className="bd-button my-5 p-2"
-            >
-              {t("settings.shutdown")}
-            </button>
-            <p className="text-center text-sm italic">
-              {t("setup.sync_restartinfo")}
-            </p>
-          </article>
+                  {t("setup.sync_wallet_unlock")}
+                </ButtonWithSpinner>
+              </form>
+
+              {error && <Message message={error} />}
+            </>
+          )} */}
         </div>
+
+        <article className="flex flex-col items-center justify-center gap-10">
+          <Button
+            type="button"
+            onClick={() => callback("shutdown", null)}
+            color="primary"
+            className="rounded-full px-8 py-6 font-semibold"
+            title={t("setup.sync_restartinfo")}
+          >
+            {t("settings.shutdown")}
+          </Button>
+        </article>
       </section>
     </SetupContainer>
   );
