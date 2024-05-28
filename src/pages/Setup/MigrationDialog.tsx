b40431139185dfc6810860979f6fd473d874d1cc
escapedcat <github@htmlcss.de>
2024-05-28 21:52:57 +0200

    feat(syncScreen): design, use nextui (#722)
    * feat(syncScreen): design, use nextui
    
    * feat(syncScreen): move wallet unlock to modal
    
    - refactor: use nextui alert instead of own message-component
    
    * feat(syncScreen): use alert for LN-wallet info
    
    * feat(syncScreen): i18n and cleanup
    
    * chore: cleanup
    
    * fix(syncScreen): design shutdown button
    
    * fix(syncScreen): adjust unlock button design

--- src/pages/Setup/MigrationDialog.tsx
+++ src/pages/Setup/MigrationDialog.tsx
@@ -1,4 +1,3 @@
-import { CloudArrowDownIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import { FC, useState } from "react";
 import { useTranslation } from "react-i18next";
 import ConfirmModal from "@/components/ConfirmModal";
