70a2dd138badb53d44624b063f8e1f840b5b5eb2
escapedcat <github@htmlcss.de>
2024-07-01 11:17:13 +0200

    chore: add playwright setup and basic test

--- README.md
+++ README.md
@@ -62,6 +62,20 @@ npm run start
 
 Then go to `http://localhost:3000` and use the password `password`.
 
+### Testing
+
+#### Unit tests
+
+`npm test`
+
+#### E2E tests
+
+Run chormium tests only:\
+`npx playwright test --project=chromium`
+
+Run test with UI:\
+`npx playwright test --project=chromium --ui`
+
 ##### Blitz API
 
 This guide uses Polar for easier development, but you can also use a real lightning node.
---
+++ tests/setup.spec.ts
@@ -0,0 +1,47 @@
+import { test } from "@playwright/test";
+
+test("happy path setup", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  // setup coice
+  await page.getByLabel("Fresh Setup").click({ force: true }); // label is somehow covered
+  await page.getByRole("button", { name: "Continue" }).click();
+
+  // delete data ingo
+  await page.getByRole("button", { name: "Yes, delete all data" }).click();
+
+  // node name
+  await page.getByLabel("Node Name").fill("BlueNode");
+  await page.getByRole("button", { name: "Continue" }).click();
+
+  // implementation choice
+  await page
+    .getByLabel("Bitcoin Full Node with Lightning LND Implementation ")
+    .click({ force: true });
+  await page.getByRole("button", { name: "Continue" }).click();
+
+  // password node
+  await page.getByLabel("Node Password", { exact: true }).fill("node12345");
+  await page
+    .getByLabel("Repeat Node Password", { exact: true })
+    .fill("node12345");
+  await page.getByRole("button", { name: "Continue" }).click();
+
+  // password apps
+  await page.getByAltText("Set your Apps password");
+  await page.getByLabel("Apps Password", { exact: true }).fill("apps12345");
+  await page
+    .getByLabel("Repeat Apps Password", { exact: true })
+    .fill("apps12345");
+  await page.getByRole("button", { name: "Continue" }).click();
+
+  // password wallet
+  await page.getByLabel("Wallet Password", { exact: true }).fill("wallet12345");
+  await page
+    .getByLabel("Repeat Wallet Password", { exact: true })
+    .fill("wallet12345");
+  await page.getByRole("button", { name: "Continue" }).click();
+
+  // ready to setup
+  await page.getByRole("button", { name: "Start setup" }).click();
+});
