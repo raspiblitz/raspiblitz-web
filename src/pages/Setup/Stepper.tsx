587b0353cbfa0df0c41f45cb2d2265e3dcc59da3
escapedcat <github@htmlcss.de>
2024-07-08 15:27:17 +0200

    refactor(stepper): rework design and border

--- src/pages/Setup/Stepper.tsx
+++ src/pages/Setup/Stepper.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@nextui-org/react";
 import { useTranslation } from "react-i18next";
 
 const NUMBER_OF_STEPS = 4;
@@ -10,25 +11,33 @@ export default function Stepper({ currentStep }: Props) {
   const { t } = useTranslation();
 
   return (
-    <div className="w-full">
-      <ul className="lg:flex hidden justify-center items-center gap-4">
+    <div
+      className={cn(
+        "w-full",
+        "after:block after:relative after:bottom-5 after:border after:border-gray-700 after:content-['']",
+      )}
+    >
+      <ul
+        className={cn(
+          "lg:flex hidden justify-between items-center relative z-10",
+        )}
+      >
         {Array.from({ length: NUMBER_OF_STEPS }).map((_, index: number) => (
-          <>
-            <li
-              className={`list-none text-center flex flex-col ${index < currentStep ? "opacity-100" : "opacity-30"}`}
-              key={index}
+          <li className={cn("list-none px-4 bg-primary-900")} key={index}>
+            <span
+              className={cn(
+                "text-center flex flex-col",
+                index >= currentStep && "opacity-30",
+              )}
             >
-              <span className="font-bold">
+              <span className="font-bold whitespace-nowrap">
                 {t(`setup.step.step`)} {index + 1}
               </span>
-              <span className="text-sm">
+              <span className="text-sm text-secondary">
                 {t(`setup.step.step_${index + 1}_description`)}
               </span>
-            </li>
-            {index < NUMBER_OF_STEPS - 1 && (
-              <div className="border-b w-16"></div>
-            )}
-          </>
+            </span>
+          </li>
         ))}
       </ul>
     </div>
