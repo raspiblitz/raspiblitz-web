b77dba070a6d6f5fcb20fd98d7a88655f1824f5e
escapedcat <github@htmlcss.de>
2024-07-09 11:22:29 +0200

    refactor(stepper): avoid cn where it has no use

--- src/pages/Setup/Stepper.tsx
+++ src/pages/Setup/Stepper.tsx
@@ -17,13 +17,9 @@ export default function Stepper({ currentStep }: Props) {
         "after:block after:relative after:bottom-5 after:border after:border-gray-700 after:content-['']",
       )}
     >
-      <ul
-        className={cn(
-          "lg:flex hidden justify-between items-center relative z-10",
-        )}
-      >
+      <ul className="lg:flex hidden justify-between items-center relative z-10">
         {Array.from({ length: NUMBER_OF_STEPS }).map((_, index: number) => (
-          <li className={cn("list-none px-4 bg-primary-900")} key={index}>
+          <li className="list-none px-4 bg-primary-900" key={index}>
             <span
               className={cn(
                 "text-center flex flex-col",
