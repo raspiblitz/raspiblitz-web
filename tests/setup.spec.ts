0acd17a7f5a870c0a8b3e580d54227439bb7b513
escapedcat <github@htmlcss.de>
2024-07-16 13:29:28 +0200

    test(setup): fix mocking

--- tests/setup.spec.ts
+++ tests/setup.spec.ts
@@ -2,26 +2,22 @@ import { test } from "@playwright/test";
 
 test("simple setup path", async ({ page }) => {
   await page.route("**/api/setup/status", async (route) => {
-    const json = [
-      {
-        setupPhase: "setup",
-        state: "waitsetup",
-        message: "Node Running",
-        initialsync: "",
-      },
-    ];
+    const json = {
+      setupPhase: "setup",
+      state: "waitsetup",
+      message: "Node Running",
+      initialsync: "",
+    };
     await route.fulfill({ json });
   });
 
   await page.route("**/api/setup/setup-start-info", async (route) => {
-    const json = [
-      {
-        setupPhase: "setup",
-        hddGotBlockchain: "0",
-        hddGotMigrationData: null,
-        migrationMode: null,
-      },
-    ];
+    const json = {
+      setupPhase: "setup",
+      hddGotBlockchain: "0",
+      hddGotMigrationData: null,
+      migrationMode: null,
+    };
     await route.fulfill({ json });
   });
 
@@ -29,43 +25,114 @@ test("simple setup path", async ({ page }) => {
 
   // setup choice
   await page.getByLabel("Fresh Setup").click({ force: true }); // label is somehow covered
-  // await page.getByRole("button", { name: "Continue" }).click();
+  await page.getByRole("button", { name: "Continue" }).click();
 
-  // // delete data info
-  // await page.getByRole("button", { name: "Yes, delete all data" }).click();
+  // delete data info
+  await page.getByRole("button", { name: "Yes, delete all data" }).click();
 
-  // // node name
-  // await page.getByLabel("Node Name").fill("BlueNode");
-  // await page.getByRole("button", { name: "Continue" }).click();
+  // node name
+  await page.getByLabel("Node Name").fill("BlueNode");
+  await page.getByRole("button", { name: "Continue" }).click();
 
-  // // implementation choice
-  // await page
-  //   .getByLabel("Bitcoin Full Node with Lightning LND Implementation ")
-  //   .click({ force: true });
-  // await page.getByRole("button", { name: "Continue" }).click();
+  // implementation choice
+  await page
+    .getByLabel("Bitcoin Full Node with Lightning LND Implementation ")
+    .click({ force: true });
+  await page.getByRole("button", { name: "Continue" }).click();
 
-  // // password node
-  // await page.getByLabel("Node Password", { exact: true }).fill("node12345");
-  // await page
-  //   .getByLabel("Repeat Node Password", { exact: true })
-  //   .fill("node12345");
-  // await page.getByRole("button", { name: "Continue" }).click();
+  // password node
+  await page.getByLabel("Node Password", { exact: true }).fill("node12345");
+  await page
+    .getByLabel("Repeat Node Password", { exact: true })
+    .fill("node12345");
+  await page.getByRole("button", { name: "Continue" }).click();
 
-  // // password apps
-  // await page.getByAltText("Set your Apps password");
-  // await page.getByLabel("Apps Password", { exact: true }).fill("apps12345");
-  // await page
-  //   .getByLabel("Repeat Apps Password", { exact: true })
-  //   .fill("apps12345");
-  // await page.getByRole("button", { name: "Continue" }).click();
+  // password apps
+  await page.getByAltText("Set your Apps password");
+  await page.getByLabel("Apps Password", { exact: true }).fill("apps12345");
+  await page
+    .getByLabel("Repeat Apps Password", { exact: true })
+    .fill("apps12345");
+  await page.getByRole("button", { name: "Continue" }).click();
 
-  // // password wallet
-  // await page.getByLabel("Wallet Password", { exact: true }).fill("wallet12345");
-  // await page
-  //   .getByLabel("Repeat Wallet Password", { exact: true })
-  //   .fill("wallet12345");
-  // await page.getByRole("button", { name: "Continue" }).click();
+  // password wallet
+  await page.getByLabel("Wallet Password", { exact: true }).fill("wallet12345");
+  await page
+    .getByLabel("Repeat Wallet Password", { exact: true })
+    .fill("wallet12345");
+  await page.getByRole("button", { name: "Continue" }).click();
 
-  // // ready to setup
-  // await page.getByRole("button", { name: "Start setup" }).click();
+  // wait for "final" api calls
+
+  await page.route("**/api/setup/setup-start-done", async (route) => {
+    const json = "STATUS";
+    await route.fulfill({ json });
+  });
+
+  await page.route("**/api/setup/status", async (route) => {
+    const json = {
+      setupPhase: "setup",
+      state: "waitsetup",
+      message: "Node Running",
+      initialsync: "",
+    };
+    await route.fulfill({ json });
+  });
+  // ready to setup
+  await page.getByRole("button", { name: "Start setup" }).click();
+  await page.route("**/api/setup/status", async (route) => {
+    const json = {
+      setupPhase: "setup",
+      state: "waitsetup",
+      message: "Node Running",
+      initialsync: "",
+    };
+    await route.fulfill({ json });
+  });
+  await page.route("**/api/setup/setup-start-info", async (route) => {
+    const json = {
+      setupPhase: "setup",
+      hddGotBlockchain: "0",
+      hddGotMigrationData: null,
+      migrationMode: null,
+    };
+    await route.fulfill({ json });
+  });
+  await page.route("**/api/setup/status", async (route) => {
+    const json = {
+      setupPhase: "setup",
+      state: "waitsetup",
+      message: "Node Running",
+      initialsync: "",
+    };
+    await route.fulfill({ json });
+  });
+  await page.route("**/api/setup/setup-start-info", async (route) => {
+    const json = {
+      setupPhase: "setup",
+      hddGotBlockchain: "0",
+      hddGotMigrationData: null,
+      migrationMode: null,
+    };
+    await route.fulfill({ json });
+  });
+  await page.route("**/api/setup/status", async (route) => {
+    const json = {
+      setupPhase: "setup",
+      state: "waitsetup",
+      message: "Node Running",
+      initialsync: "",
+    };
+    await route.fulfill({ json });
+  });
+
+  await page.route("**/api/setup/setup-start-info", async (route) => {
+    const json = {
+      setupPhase: "setup",
+      hddGotBlockchain: "0",
+      hddGotMigrationData: null,
+      migrationMode: null,
+    };
+    await route.fulfill({ json });
+  });
 });
