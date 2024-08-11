f41bc14490908ea9914e1803877f762720d494c1
escapedcat <github@htmlcss.de>
2024-08-11 16:03:07 +0800

    fix(settings): avoid i18n text and select being too close

--- src/pages/Settings/I18nBox.tsx
+++ src/pages/Settings/I18nBox.tsx
@@ -11,7 +11,7 @@ const I18nBox: FC = () => {
   return (
     <div className="box-border w-full transition-colors text-white">
       <article className="relative rounded p-5 shadow-xl bg-gray-800">
-        <div className="flex justify-between">
+        <div className="flex justify-between gap-2">
           <h4 className="flex w-1/2 items-center font-bold xl:w-2/3">
             {t("settings.curr_lang")}
           </h4>
