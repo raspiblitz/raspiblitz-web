fd3e08b1faa759af7faad48d49def4ff35e25cf1
Christoph Stenglein <christoph@cstenglein.com>
2024-10-05 11:00:15 +0200

    feat: adapt button to suggestions

--- src/layouts/SetupContainer.tsx
+++ src/layouts/SetupContainer.tsx
@@ -1,7 +1,7 @@
+import { Button } from "@/components/Button";
 import I18nSelect from "@/components/I18nDropdown";
 import Stepper from "@/pages/Setup/Stepper";
 import { BookOpenIcon } from "@heroicons/react/24/outline";
-import { Chip } from "@nextui-org/react";
 import { PropsWithChildren } from "react";
 
 type Props = {
@@ -15,15 +15,18 @@ export default function SetupContainer({
   return (
     <main className="flex min-h-screen h-full w-screen flex-col items-center justify-center bg-primary-900 transition-colors text-white">
       <div className="fixed md:right-16 top-16 md:top-8 flex h-8 w-48 md:w-96 md:flex-row flex-col-reverse gap-6 md:gap-4 justify-center items-center">
-        <a href="https://docs.raspiblitz.org/" target="_blank" rel="noreferrer">
-          <Chip
-            color="primary"
-            className="p-4"
-            startContent={<BookOpenIcon className="w-5 h-5" />}
-          >
-            Documentation
-          </Chip>
-        </a>
+        <Button
+          as="a"
+          href="https://docs.raspiblitz.org/"
+          target="_blank"
+          rel="noreferrer"
+          color="primary"
+          variant="ghost"
+          className="p-4 w-full"
+          startContent={<BookOpenIcon className="w-5 h-5" />}
+        >
+          Documentation
+        </Button>
         <I18nSelect />
       </div>
 
