16dcd5c04724bf8ba748e30d9bd061f32f3e00c7
escapedcat <github@htmlcss.de>
2024-10-30 14:58:50 +0100

    refactor: switch spinnerbutton and spinner for nextui components

--- src/layouts/LoadingScreen.tsx
+++ src/layouts/LoadingScreen.tsx
@@ -1,12 +1,12 @@
-import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner";
 import RaspiBlitzLogoDark from "@/assets/RaspiBlitz_Logo_Main_Negative.svg?react";
+import { Spinner } from "@nextui-org/react";
 
 // Loading Screen for the initial loading of the app
 export default function LoadingScreen() {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center bg-gray-700">
       <RaspiBlitzLogoDark className="mb-5 h-12" />
-      <LoadingSpinner color="text-yellow-500" />
+      <Spinner size="lg" />
     </main>
   );
 }
