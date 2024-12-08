b0b85c2bd7a8127ea1487f159c8146d90e1faf4a
escapedcat <github@htmlcss.de>
2024-12-08 18:39:55 +0100

    style: format

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,58 +7,58 @@ import { render, waitFor, screen } from "test-utils";
 const mockedUsedNavigate = vi.fn();
 
 vi.mock("react-router-dom", async () => {
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  const reactRouterDom: any = await vi.importActual("react-router-dom");
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	const reactRouterDom: any = await vi.importActual("react-router-dom");
 
-  return { ...reactRouterDom, useNavigate: () => mockedUsedNavigate };
+	return { ...reactRouterDom, useNavigate: () => mockedUsedNavigate };
 });
 
 describe("App", () => {
-  afterEach(() => {
-    server.resetHandlers();
-  });
+	afterEach(() => {
+		server.resetHandlers();
+	});
 
-  test("should route to /setup if setup is not done", async () => {
-    server.use(
-      http.get("/api/setup/status", () => {
-        return HttpResponse.json(
-          {
-            setupPhase: "starting",
-            state: "",
-            message: "",
-          },
-          { status: 200 }
-        );
-      })
-    );
+	test("should route to /setup if setup is not done", async () => {
+		server.use(
+			http.get("/api/setup/status", () => {
+				return HttpResponse.json(
+					{
+						setupPhase: "starting",
+						state: "",
+						message: "",
+					},
+					{ status: 200 },
+				);
+			}),
+		);
 
-    render(
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    );
-    await waitFor(() => expect(mockedUsedNavigate).toHaveBeenCalledTimes(1));
-  });
+		render(
+			<I18nextProvider i18n={i18n}>
+				<App />
+			</I18nextProvider>,
+		);
+		await waitFor(() => expect(mockedUsedNavigate).toHaveBeenCalledTimes(1));
+	});
 
-  test("should route to /login if setup is done", async () => {
-    server.use(
-      http.get("/api/setup/status", () => {
-        return HttpResponse.json(
-          {
-            setupPhase: "done",
-            state: "",
-            message: "",
-          },
-          { status: 200 }
-        );
-      })
-    );
+	test("should route to /login if setup is done", async () => {
+		server.use(
+			http.get("/api/setup/status", () => {
+				return HttpResponse.json(
+					{
+						setupPhase: "done",
+						state: "",
+						message: "",
+					},
+					{ status: 200 },
+				);
+			}),
+		);
 
-    render(
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    );
-    expect(await screen.findByText("login.login")).toBeInTheDocument();
-  });
+		render(
+			<I18nextProvider i18n={i18n}>
+				<App />
+			</I18nextProvider>,
+		);
+		expect(await screen.findByText("login.login")).toBeInTheDocument();
+	});
 });
