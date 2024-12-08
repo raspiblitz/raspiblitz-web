03c99dabbf5acbc4cc0b4c42381accbdcedd39b1
escapedcat <github@htmlcss.de>
2024-12-08 18:36:49 +0100

    chore(home): qr-code linting

--- src/pages/Home/ReceiveModal/QRCode.tsx
+++ src/pages/Home/ReceiveModal/QRCode.tsx
@@ -1,47 +1,50 @@
 import { Button } from "@/components/Button";
 import useClipboard from "@/hooks/use-clipboard";
 import { QRCodeSVG } from "qrcode.react";
-import { FC } from "react";
+import type { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { Tooltip } from "react-tooltip";
 
 type Props = {
-  address: string;
-  onRefreshHandler?: () => void;
+	address: string;
+	onRefreshHandler?: () => void;
 };
 
 const ReceiveOnChain: FC<Props> = ({ address, onRefreshHandler }) => {
-  const { t } = useTranslation();
-  const [copyAddress, addressCopied] = useClipboard(address);
+	const { t } = useTranslation();
+	const [copyAddress, addressCopied] = useClipboard(address);
 
-  return (
-    <>
-      <QRCodeSVG role={"img"} className="mx-auto" value={address} size={256} />
+	return (
+		<>
+			<QRCodeSVG role={"img"} className="mx-auto" value={address} size={256} />
 
-      <p className="my-5 text-sm text-gray-300">{t("wallet.scan_qr")}</p>
+			<p className="my-5 text-sm text-gray-300">{t("wallet.scan_qr")}</p>
 
-      <article className="mb-5 flex flex-row items-center">
-        <p
-          onClick={copyAddress}
-          className="w-full break-all text-gray-200"
-          data-tooltip-id="copy-tooltip"
-        >
-          {address}
-        </p>
-        <Tooltip id="copy-tooltip">
-          <div>
-            {addressCopied ? t("wallet.copied") : t("wallet.copy_clipboard")}
-          </div>
-        </Tooltip>
-      </article>
+			<article className="mb-5 flex flex-row items-center">
+				<p
+					onClick={copyAddress}
+					onKeyUp={(e) => {
+						if (e.key === "Enter") copyAddress();
+					}}
+					className="w-full break-all text-gray-200"
+					data-tooltip-id="copy-tooltip"
+				>
+					{address}
+				</p>
+				<Tooltip id="copy-tooltip">
+					<div>
+						{addressCopied ? t("wallet.copied") : t("wallet.copy_clipboard")}
+					</div>
+				</Tooltip>
+			</article>
 
-      {onRefreshHandler && (
-        <div>
-          <Button onClick={onRefreshHandler}>{t("wallet.refresh")}</Button>
-        </div>
-      )}
-    </>
-  );
+			{onRefreshHandler && (
+				<div>
+					<Button onClick={onRefreshHandler}>{t("wallet.refresh")}</Button>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default ReceiveOnChain;
