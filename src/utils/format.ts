98bb6ba9e78433f031c1a2e1528d6ef27f3c9fd6
escapedcat <github@htmlcss.de>
2024-12-08 18:37:58 +0100

    style: format

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -4,30 +4,30 @@ export const SAT_DENOMINATOR = 100_000_000;
 export const NUM_LOCALE = "en-US";
 
 export function convertSatToBtc(sat: number | null): number | null {
-  return sat ? sat / SAT_DENOMINATOR : null;
+	return sat ? sat / SAT_DENOMINATOR : null;
 }
 
 export function convertBtcToSat(btc: number): number {
-  return btc * SAT_DENOMINATOR;
+	return btc * SAT_DENOMINATOR;
 }
 
 export function convertMSatToSat(mSat: number | null): number | null {
-  return mSat ? mSat / 1000 : null;
+	return mSat ? mSat / 1000 : null;
 }
 
 export function convertMSatToBtc(mSat: number | null): number | null {
-  return convertSatToBtc(mSat ? mSat / 1000 : null);
+	return convertSatToBtc(mSat ? mSat / 1000 : null);
 }
 
 export function convertToString(unit: Unit, num: number | null): string {
-  if (num === null) {
-    return "-";
-  }
-  if (unit === Unit.SAT) {
-    // remove decimals from sat & convert
-    return (+num.toFixed(0)).toLocaleString(NUM_LOCALE);
-  }
-  return num.toFixed(8).toString();
+	if (num === null) {
+		return "-";
+	}
+	if (unit === Unit.SAT) {
+		// remove decimals from sat & convert
+		return (+num.toFixed(0)).toLocaleString(NUM_LOCALE);
+	}
+	return num.toFixed(8).toString();
 }
 
 /**
@@ -38,33 +38,33 @@ export function convertToString(unit: Unit, num: number | null): string {
  */
 // TODO: add test for this function to format.test.ts
 export function formatAmount(value: string, unit: Unit): string {
-  // replace every character except numbers and separators
-  let formattedValue = value.replace(/[^0-9.,]/, "");
+	// replace every character except numbers and separators
+	let formattedValue = value.replace(/[^0-9.,]/, "");
 
-  if (unit === Unit.SAT) {
-    // remove all separators to format correctly
-    formattedValue = formattedValue.replace(/,|\./g, "");
-    if (formattedValue) {
-      formattedValue = new Intl.NumberFormat(NUM_LOCALE).format(
-        +formattedValue
-      );
-    }
-  } else {
-    // remove commas
-    formattedValue = formattedValue.replace(/,/g, "");
-    // replace ".." with "."
-    formattedValue = formattedValue.replace(/\.\./g, ".");
-    const output = formattedValue.split(".");
-    // limit to max 8 decimal places
-    if (output[1]?.length > 8) {
-      output[1] = output[1].substring(0, 8);
-    }
-    // formatting which respects separator
-    // makes either "x.y" or "y"
-    formattedValue =
-      output.shift() + (output.length ? `.${output.join("")}` : "");
-  }
-  return formattedValue;
+	if (unit === Unit.SAT) {
+		// remove all separators to format correctly
+		formattedValue = formattedValue.replace(/,|\./g, "");
+		if (formattedValue) {
+			formattedValue = new Intl.NumberFormat(NUM_LOCALE).format(
+				+formattedValue,
+			);
+		}
+	} else {
+		// remove commas
+		formattedValue = formattedValue.replace(/,/g, "");
+		// replace ".." with "."
+		formattedValue = formattedValue.replace(/\.\./g, ".");
+		const output = formattedValue.split(".");
+		// limit to max 8 decimal places
+		if (output[1]?.length > 8) {
+			output[1] = output[1].substring(0, 8);
+		}
+		// formatting which respects separator
+		// makes either "x.y" or "y"
+		formattedValue =
+			output.shift() + (output.length ? `.${output.join("")}` : "");
+	}
+	return formattedValue;
 }
 
 /**
@@ -74,6 +74,6 @@ export function formatAmount(value: string, unit: Unit): string {
  * @returns the converted number, e.g. 123456
  */
 export function stringToNumber(value: string): number {
-  const valueCopy = value.replace(/,/g, "");
-  return +valueCopy;
+	const valueCopy = value.replace(/,/g, "");
+	return +valueCopy;
 }
